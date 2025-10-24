38d8c0b30c9afb5afb4efc84f4d7cb6c77ab695b
Mario Iliescu <355142@via.dk>
2025-10-24 16:30:03 +0200

    Added NetworkHandlers and revised proto

--- Server/GrpcAPI/FleetMainGrpcHandler.cs
+++ Server/GrpcAPI/FleetMainGrpcHandler.cs
@@ -2,15 +2,16 @@
 using Grpc.Net.Client;
 using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
-using GrpcClientApp.Protos;
+using GrpcAPI.Protos;
 
-namespace GrpcClientApp.Services
+namespace GrpcAPI
 {
     public class FleetMainGrpcHandler
     {
         private readonly FleetService.FleetServiceClient _client;
-
-        public FleetMainGrpcHandler(GrpcChannel channel)
+        private const string Host = "http://localhost:6032";
+        GrpcChannel channel = GrpcChannel.ForAddress(Host);
+        public FleetMainGrpcHandler()
         {
             _client = new FleetService.FleetServiceClient(channel);
         }
@@ -18,19 +19,10 @@ namespace GrpcClientApp.Services
         /// <summary>
         /// Sends a request to the gRPC server specifying the handler and action
         /// </summary>
-        /// <param name="handler">Which handler the server should use</param>
-        /// <param name="action">Action type</param>
-        /// <param name="payload">Any protobuf message to send</param>
+        /// <param name="request"></param>
         /// <returns>The server response</returns>
-        public async Task<Response> SendRequestAsync(HandlerType handler, ActionType action, IMessage payload)
+        public async Task<ResponseProto> SendRequestAsync(RequestProto request)
         {
-            var request = new Request
-            {
-                Handler = handler,
-                Action = action,
-                Payload = Any.Pack(payload)
-            };
-
             try
             {
                 return await _client.SendRequestAsync(request);
