e739022980e61a754af9f68e13e6ea2205fd315c
patri <patrik.golian888@gmail.com>
2025-11-19 11:54:44 +0100

    DriverDto, CreateDriverDto, create and getDriverById webapi methods

---
+++ Shared/ApiContracts/Dtos/Driver/DriverDto.cs
@@ -0,0 +1,23 @@
+﻿using ApiContracts.Enums;
+
+namespace ApiContracts.Dtos.Driver;
+
+public record DriverDto(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string PhoneNumber,
+    string Password,
+
+    // User role (forced to DRIVER by builder but included for completeness)
+    UserRole Role,
+
+    // Driver-specific fields
+    string McNumber,
+    DriverStatus Status,
+    TrailerType TrailerType,
+    string LocationState,
+    int LocationZip,
+    DriverCompanyRole CompanyRole
+);
