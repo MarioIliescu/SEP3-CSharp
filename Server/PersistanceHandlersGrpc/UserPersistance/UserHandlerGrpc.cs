047e3979e321b4eddc3e320a9bcabe6a0e940036
Mario Iliescu <355142@via.dk>
2025-11-19 14:08:17 +0100

    Added HandleDispatcher

--- Server/PersistanceHandlersGrpc/UserPersistance/UserHandlerGrpc.cs
+++ Server/PersistanceHandlersGrpc/UserPersistance/UserHandlerGrpc.cs
@@ -12,12 +12,15 @@ namespace PersistanceHandlersGrpc.UserPersistance;
 public class UserHandlerGrpc : IFleetPersistanceHandler
 {
     private readonly IDriverRepository _driverRepository;
+    private readonly IDispatcherRepository _dispatcherRepository;
     private ILogger<UserHandlerGrpc> _logger;
 
     public UserHandlerGrpc([FromKeyedServices("user")] IDriverRepository driverRepository,
+        IDispatcherRepository dispatcherRepository,
         ILogger<UserHandlerGrpc> logger)
     {
         _driverRepository = driverRepository;
+        _dispatcherRepository = dispatcherRepository;
         _logger = logger;
     }
     public async Task<object> HandleAsync(Request request)
@@ -29,8 +32,8 @@ public class UserHandlerGrpc : IFleetPersistanceHandler
                 _logger.LogInformation($"Handling driver {payload}");
                 return await HandleDriverAsync(request.Action, payload);
             case HandlerType.Dispatcher:
-                //TODO Implement this
-                return new NotImplementedException();
+                Dispatcher dispatcher = request.Payload as Dispatcher;
+                return await HandleDispatcherAsync(request.Action, dispatcher);
             default:
                 _logger.LogError($"Unhandled handler type {request.Handler}");
                 throw new NotImplementedException();
@@ -60,6 +63,35 @@ public class UserHandlerGrpc : IFleetPersistanceHandler
             case ActionType.List:
                 _logger.LogInformation($"Getting drivers {request}");
                 return  _driverRepository.GetManyAsync();
+            default:
+                _logger.LogError($"Unknown action type {actionType}");
+                throw new InvalidEnumArgumentException("Unknown action type");
+        }
+        return Task.CompletedTask;
+    }
+    private async Task<object> HandleDispatcherAsync(ActionType actionType, Dispatcher request)
+    {
+        switch (actionType)
+        {
+            case ActionType.Create:
+                _logger.LogInformation($"Creating dispatcher {request}");
+                return await _dispatcherRepository.CreateAsync(request);
+            case ActionType.Update:
+                _logger.LogInformation($"Updating dispatcher {request}");
+                await  _dispatcherRepository.UpdateAsync(request);
+                _logger.LogInformation($"Updated dispatcher {request}");
+                break;
+            case ActionType.Delete:
+                _logger.LogInformation("$Trying to delete dispatcher {request}");
+                await _dispatcherRepository.DeleteAsync(request.Id);
+                _logger.LogInformation($"Deleted driver with Id{request.Id}");
+                break;
+            case ActionType.Get:
+                _logger.LogInformation($"Fetching dispatcher {request}");
+                return await _dispatcherRepository.GetSingleAsync(request.Id);
+            case ActionType.List:
+                _logger.LogInformation($"Getting dispatchers {request}");
+                return  _dispatcherRepository.GetManyAsync();
             default:
                 _logger.LogError($"Unknown action type {actionType}");
                 throw new InvalidEnumArgumentException("Unknown action type");
