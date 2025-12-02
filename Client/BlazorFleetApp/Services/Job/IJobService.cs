b20e19f75803f39d1b4d456e5f9e0ebd6ef76754
patri <patrik.golian888@gmail.com>
2025-12-02 09:46:43 +0100

    Http client for Job

---
+++ Client/BlazorFleetApp/Services/Job/IJobService.cs
@@ -0,0 +1,12 @@
+﻿using ApiContracts.Dtos.Job;
+
+namespace BlazorFleetApp.Services.Job;
+
+public interface IJobService
+{
+    public Task CreateAsync(CreateJobDto dto);
+    public Task UpdateAsync(JobDto dto);
+    public Task<JobDto?> GetSingleAsync(int id);
+    public Task<List<JobDto>> GetAllAsync();
+    public Task DeleteAsync(int id);   
+}
