0dc6382e34e3e3adeae6564e114d2efbbc780ade
Mihail Rotaru <355401@via.dk>
2025-12-03 13:10:57 +0100

    Added Task to the return of the methods

--- Server/RepositoryContracts/IRecruitDriverRepository.cs
+++ Server/RepositoryContracts/IRecruitDriverRepository.cs
@@ -4,8 +4,8 @@ namespace Repositories;
 
 public interface IRecruitDriverRepository
 {
-    Driver RecruitDriverAsync(Driver driver, Dispatcher dispatcher);
-    void FireDriverAsync(Driver driver, Dispatcher dispatcher);
+    Task<Driver> RecruitDriverAsync(Driver driver, Dispatcher dispatcher);
+    Task FireDriverAsync(Driver driver, Dispatcher dispatcher);
     IQueryable<Driver> GetDispatcherDriversListAsync(int id);
     IQueryable<Driver> GetDriverListWoDispatcherAsync();
 }
