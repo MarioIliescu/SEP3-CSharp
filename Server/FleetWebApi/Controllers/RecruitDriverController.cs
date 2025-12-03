3f578451eaaa146104fc5b9cd36f6e0d03e2428b
Carolina Valencia Zavidei <zavidei.carolina@gmail.com>
2025-12-03 15:02:09 +0100

    recruit driver stuff

---
+++ Server/FleetWebApi/Controllers/RecruitDriverController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Driver;
+using Services.RecruitDriver;
+
+namespace FleetWebApi.Controllers;
+
+
+[ApiController]
+[Route("RecruitDriver")]
+public class RecruitDriverController : ControllerBase
+{
+    private readonly IRecruitDriverService _recruitDriverService;
+    
+    public RecruitDriverController(IRecruitDriverService recruitDriverService)
+    {
+        _recruitDriverService = recruitDriverService;
+    }
+    
+    
+}
---
+++ Server/Services/RecruitDriver/RecruitDriverService.cs
@@ -0,0 +1,34 @@
+﻿using ApiContracts.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using PersistanceContracts;
+using Entities.Driver;
+namespace Services.RecruitDriver;
+
+
+public class RecruitDriverService : IRecruitDriverService
+{
+    private readonly IFleetPersistanceHandler _handler;
+    
+    public RecruitDriverService(
+        [FromKeyedServices(HandlerType.Job)] IFleetPersistanceHandler handler)
+    {
+        _handler = handler;
+    }
+
+    public Task<Driver> RecruitDriverAsync(Driver driver, Dispatcher dispatcher)
+    {
+        throw new NotImplementedException();
+    }
+    public Task FireDriverAsync(Driver driver, Dispatcher dispatcher)
+    {
+        throw new NotImplementedException();
+    }
+    public IQueryable<Driver> GetDispatcherDriversListAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public IQueryable<Driver> GetDriverListWoDispatcherAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
