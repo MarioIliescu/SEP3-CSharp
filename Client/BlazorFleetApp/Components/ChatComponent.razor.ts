a0b5f9b9f45bc5404931137a8d80ae5e1fc2a31b
Mario Iliescu <355142@via.dk>
2025-12-11 15:58:37 +0100

    Added job component, fixed some issues, added javascript for smoothness and proper logic, added baka description when necesarry with on click bs.

--- Client/BlazorFleetApp/Components/ChatComponent.razor.ts
+++
@@ -1,8 +0,0 @@
-export function scrollToBottom(element: HTMLElement) {
-    if (element) {
-        element.scrollTop = element.scrollHeight;
-    }
-}
-
-// expose globally
-(window as any).blazorScrollToBottom = scrollToBottom;
